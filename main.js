87d2df9d34e3e9a57f8666fa3fbaf91956de2a60
bungees <84727439+bungeees@users.noreply.github.com>
2024-04-09 14:29:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+  const canvas = document.getElementById('lienzo');
+  const ctx = canvas.getContext('2d');
+  let painting = false;
 
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+
+  function finishedPosition() {
+    painting = false;
+    ctx.beginPath();
+  }
+
+  function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+  }
+
+  canvas.addEventListener('mousedown', startPosition);
+  canvas.addEventListener('mouseup', finishedPosition);
+  canvas.addEventListener('mousemove', draw);
